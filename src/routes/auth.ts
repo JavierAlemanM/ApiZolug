7f1b6128ca045a35ed6a91d3f79625ec985552aa
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-11-30 14:43:40 -0500

    iniciamos migracion a NestJS

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { authController } from "../controllers/auth";
-
-const router = express.Router();
-
-router.post("/auth", authController.login);
-
-export default router;
