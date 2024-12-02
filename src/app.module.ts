0a44ea39abab79ed071ba85333a0cbed530c595a
Deyler Mena <133424744+KeniBeck@users.noreply.github.com>
2024-12-02 23:18:20 +0000

    creacion de recursos login & user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { LoginModule } from './login/login.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
